1860dc1e2c571d8ffadc0bf844b08bd6ff3e5071
apullo777 <peter.yaochen.chang@gmail.com>
2022-12-05 15:53:46 +0100

    refactor

--- rotate-pdf.py
+++ rotate-pdf.py
@@ -1,15 +1,22 @@
 import PyPDF2
 
-with open("input.pdf", "rb") as file:
+# The input and output file names
+INPUT_FILE = "input.pdf"
+OUTPUT_FILE = "output.pdf"
+
+# The rotation angle (in degrees)
+ANGLE = 90
+
+with open(INPUT_FILE, "rb") as file:
     pdf = PyPDF2.PdfFileReader(file)
     output = PyPDF2.PdfFileWriter()
     
     # Iterate over the pages of the PDF
     for i in range(pdf.getNumPages()):
         page = pdf.getPage(i)
-        page.rotateClockwise(90)
+        page.rotateClockwise(ANGLE)
         output.addPage(page)
     
     # Write the output PDF to a file
-    with open("output.pdf", "wb") as output_file:
+    with open(OUTPUT_FILE, "wb") as output_file:
         output.write(output_file)
